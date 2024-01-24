f730ef5e8c075760fa933a41ea1f79ea5e1e9e28
angelusMZ19 <angelomontenegro200306@gmail.com>
2024-01-24 19:10:46 +0100

    ejercicio 3

---
+++ REPAS/exercici_3.py
@@ -0,0 +1,12 @@
+#Demanar a l’usuari que introdueixi 2 valors i mostrar, per pantalla, quin és el més gran.
+
+n1 = int(input("Dame el valor1: "))
+n2 = int(input("Dame el valor2: "))
+
+
+if n1 > n2:
+    print("El más grande es:", n1)
+else:
+    print("El más grande es:", n2)
+
+    
