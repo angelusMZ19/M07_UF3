63d90a3727ab7c73b9dd2190dce0407ef93dd89e
angelusMZ19 <angelomontenegro200306@gmail.com>
2024-01-26 16:48:00 +0100

    ejercicio5

---
+++ REPAS/exercici_5.py
@@ -0,0 +1,8 @@
+#pedir un numero y determinar si el numero es par o impar
+
+valor=int(input("ingrese un valor para deterinar su caracter:\n "))
+
+if valor%2 == 0:
+    print(" es parell ")
+else:
+    print(" es senar ")
