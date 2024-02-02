93538e3704b4968566083ce9273f1acd50e02bd0
angelusMZ19 <angelomontenegro200306@gmail.com>
2024-02-02 16:08:07 +0100

    ejercicio15

--- REPAS/exercici_15.py
+++ REPAS/exercici_15.py
@@ -1,16 +1,4 @@
 # El mateix que l’anterior exercici, però sense demanar un màxim de números. L’usuari indicarà quan ha acabat posant un 0.
-# numeros = []
-# while True:
-#     try:
-#         entrada = input("Introduce un numero y (0 para salir): ")
-#         num = int(entrada)
-#         if num == 0:
-#             break
-#         numeros.append(num)
-    
-#     except ValueError:
-#         print("ERROR: Entrada no vàlida. porfavor introduzca un NUMERO")
-# print("Numero introducidos:", numeros)
 
 numeros = []
 while True:
