128b7c418f1f8d9a23c12a996fb973c013da7b55
angelusMZ19 <angelomontenegro200306@gmail.com>
2024-02-02 17:26:36 +0100

    ejercicio20 avance clase

---
+++ REPAS/exercici_20.py
@@ -0,0 +1,14 @@
+#Crear un diccionari on la clau (key) sigui un nom i el valor (value) l’edat. S’haura de demanar a l’usuari que posi contactes (noms i edats). Si algun nom es repeteix no s’fageirà al diccionari (indicant-ho a l’usuari). Es deixarà d’inserir contactes quan l’usuari indiqui que no vol afegir-ne més.
+
+myAgend={}
+while True:
+    nom=input("Ingrese un nombre (stop para finalizar o -1)")
+
+    if nom=="stop" or nom=="-1":
+        break
+    if nom in myAgend:
+        print("Nombre ya registrado, porfavor, Ingrese un nuevo registro")
+    else: 
+        edad=int(input("Ingrese edad del agendado"))
+        
+
